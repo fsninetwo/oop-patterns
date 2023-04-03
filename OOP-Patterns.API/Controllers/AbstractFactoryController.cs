423b79c27daabebc1aa526ea1f2ac6215864b601
Никита Медведев <grizzlyFS92@hotmail.com>
2023-04-03 10:31:51 +0300

    Initialize abstract factory controller

---
+++ OOP-Patterns.API/Controllers/AbstractFactoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OOP_Patterns.Common.Domain.Enums;
+
+namespace OOP_Patterns.API.Controllers
+{
+    public class AbstractFactoryController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> UploadDocument(string message, UploadEnum uploadType)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DownloadDocument(string message, UploadEnum uploadType)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ OOP-Patterns.Services/Providers/IDocumentProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Patterns.Services.Providers
+{
+    public interface IDocumentProvider
+    {
+
+    }
+}
