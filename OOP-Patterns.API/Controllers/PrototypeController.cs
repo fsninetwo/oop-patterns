2aa003a4fcb8c3687ebe36607aca17fa78c34c3f
Никита Медведев <grizzlyFS92@hotmail.com>
2023-04-05 09:40:45 +0300

    Initialize prototype pattern

---
+++ OOP-Patterns.API/Controllers/PrototypeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OOP_Patterns.Common.Domain.Enums;
+using OOP_Patterns.Services.IServices;
+
+namespace OOP_Patterns.API.Controllers
+{
+    public class PrototypeController : BaseController
+    {
+        private readonly IReportService _reportService;
+
+        public PrototypeController(IReportService reportService) 
+        { 
+            _reportService = reportService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDeepCopy(DocumentEnum documentType)
+        {
+            return Ok(await _reportService.GetDeepCopyReportAsync(documentType));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetShallowCopy(DocumentEnum documentType)
+        {
+            return Ok(await _reportService.GetShallowCopyReportAsync(documentType));
+        }
+    }
+}
