3c0c811aa0ba69463b21afa0f1ab5f74e4a48839
Никита Медведев <grizzlyFS92@hotmail.com>
2023-04-05 08:15:39 +0300

    Initialize builder pattern

---
+++ OOP-Patterns.API/Controllers/BuilderController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OOP_Patterns.Common.Domain.Enums;
+using OOP_Patterns.Services.IServices;
+
+namespace OOP_Patterns.API.Controllers
+{
+    public class BuilderController : BaseController
+    {
+        private readonly IReportService _reportService;
+
+        public BuilderController(IReportService reportService) 
+        { 
+            _reportService = reportService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetFullReport(DocumentEnum documentType)
+        {
+            return Ok(await _reportService.GetFullReportAsync(documentType));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetShortReport(DocumentEnum documentType)
+        {
+            return Ok(await _reportService.GetShortReportAsync(documentType));
+        }
+    }
+}
---
+++ OOP-Patterns.Common/Domain/Builders/ReportBuilder.cs
@@ -0,0 +1,48 @@
+﻿using OOP_Patterns.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Patterns.Common.Domain.Builders
+{
+    public class ReportBuilder
+    {
+        private readonly ReportModel reportModel;
+
+        public ReportBuilder()
+        {
+            reportModel = new ReportModel();
+        }
+
+        public ReportBuilder SetReportType(string message)
+        {
+            reportModel.Type = message;
+            return this;
+        }
+
+        public ReportBuilder SetReportContent(string message)
+        {
+            reportModel.Content = message;
+            return this;
+        }
+
+        public ReportBuilder SetReportFooter(string message)
+        {
+            reportModel.Footer = message;
+            return this;
+        }
+
+        public ReportBuilder SetReportHeader(string message)
+        {
+            reportModel.Header = message;
+            return this;
+        }
+
+        public ReportModel BuildReport()
+        {
+            return reportModel;
+        }
+    }
+}
