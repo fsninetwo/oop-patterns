266157777cea92b8d6cfc65f76ab1d5cb0d91beb
Никита Медведев <grizzlyFS92@hotmail.com>
2023-04-19 10:57:51 +0300

    Implement array aggregate

---
+++ OOP-Patterns.Common/Domain/Aggregates/Interfaces/IAggregate.cs
@@ -0,0 +1,14 @@
+﻿using OOP_Patterns.Common.Domain.Iterators.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Patterns.Common.Domain.Aggregates.Interfaces
+{
+    public interface IAggregate<T>
+    {
+        IIterator<T> CreateIterator();
+    }
+}
---
+++ OOP-Patterns.Common/Domain/Iterators/Interfaces/IIterator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Patterns.Common.Domain.Iterators.Interfaces
+{
+    public interface IIterator<T>
+    {
+        bool IsCompleted { get; }
+
+        bool MoveNext();
+
+        T Current();
+
+        void Reset();
+    }
+}
