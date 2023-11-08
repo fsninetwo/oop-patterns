22e47dbe14ec942f607fbe2d9e2315ef1d8a9625
Никита Медведев <grizzlyFS92@hotmail.com>
2023-11-08 10:24:02 +0300

    implement visitor component

---
+++ OOP-Patterns.API/Controllers/VisitorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OOP_Patterns.Services.IServices;
+
+namespace OOP_Patterns.API.Controllers
+{
+    public class VisitorController : BaseController
+    {
+        private readonly IDiagnosticsService _diagnosticsService;
+
+        public VisitorController(IDiagnosticsService diagnosticsService) 
+        { 
+            _diagnosticsService = diagnosticsService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DiagnioseComponentAsync()
+        {
+            return Ok(await _diagnosticsService.DiagnoseComponentsAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CheckComponentStatus()
+        {
+            return Ok(await _diagnosticsService.CheckComponentsStatusAsync());
+        }
+    }
+}
---
+++ OOP-Patterns.Common/Domain/Visitors/Components/RepositoryComponent.cs
@@ -0,0 +1,18 @@
+﻿using OOP_Patterns.Common.Domain.Visitors.Components.Interfaces;
+using OOP_Patterns.Common.Domain.Visitors.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Patterns.Common.Domain.Visitors.Components
+{
+    public class RepositoryComponent : IMicroserviceComponent
+    {
+        public async Task<string> AcceptAsync(IMicroserviceVisitor computerVisitor)
+        {
+            return await computerVisitor.Visit(this);
+        }
+    }
+}
